c1112aedc276dd68e25d390ecf51c2cec0bffcfe
Hemanth <hemanthbalakrishnan2002@gmail.com>
2023-09-30 11:08:02 +0530

    recent comin the error page fortoken

--- src/interceptors/token.interceptor.ts
+++ src/interceptors/token.interceptor.ts
@@ -3,15 +3,18 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpErrorResponse
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, throwError } from 'rxjs';
 import { AuthService } from 'src/services/auth.service';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
 
-  constructor(private auth:AuthService) {}
+  constructor(private auth:AuthService,private router:Router) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     const myToken=this.auth.getToken();
@@ -21,6 +24,28 @@ export class TokenInterceptor implements HttpInterceptor {
       });
     }
 
-    return next.handle(request);
+    return next.handle(request).pipe(
+      catchError((err:any)=>{
+        if(err instanceof HttpErrorResponse){
+          if(err.status===401){
+            const Toast = Swal.mixin({
+              toast: true,
+              position: 'top',
+              showConfirmButton: false,
+              timer: 2000,
+              timerProgressBar: true,
+          })
+          Toast.fire({
+              icon: 'warning',
+              title: 'Login First',
+              text:'Please login to Explore'
+          })
+      
+            this.router.navigate(['login']);
+          }
+        }
+        return throwError(()=>new Error("Some other error occured"))
+      })
+    );
   }
 }
